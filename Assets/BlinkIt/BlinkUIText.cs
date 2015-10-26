6aa21b8102395e4d1e1724e84425d3e1a6171754
孤月蓝风 <trlanfeng@foxmail.com>
2015-10-27 00:08:49 +0800

    增加BlinkIt工具，增加FlyText工具

---
+++ Assets/BlinkIt/BlinkUIText.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkUIText : MonoBehaviour
+{
+    //是否开启
+    public bool isEnable = true;
+    //fadeIn之后的显示时间
+    public float showTime = 1;
+    //fadeOut之后的隐藏时间
+    public float hideTime = 1;
+    //闪烁速度
+    public float blinkSpeed = 1;
+
+    private float timer = 0;
+    Text text;
+
+    void Start()
+    {
+        text = transform.GetComponent<Text>();
+    }
+    void Update()
+    {
+        if (isEnable)
+        {
+            timer += Time.deltaTime * blinkSpeed;
+            if (timer >= 0 && timer < 1)
+            {
+                text.color = new Color(1, 1, 1, timer);
+            }
+            else if (timer >= 1 && timer < (1 + showTime))
+            {
+                text.color = new Color(1, 1, 1, 1);
+            }
+            else if (timer >= (1 + showTime) && timer < (2 + showTime))
+            {
+                text.color = new Color(1, 1, 1, 2 + showTime - timer);
+            }
+            else if (timer >= (2 + showTime) && timer < (2 + showTime + hideTime))
+            {
+                text.color = new Color(1, 1, 1, 0);
+            }
+            else
+            {
+                timer = 0;
+            }
+        }
+    }
+}
---
+++ Assets/FlyText/FlyText.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FlyText : MonoBehaviour
+{
+    public float distance = 50;
+    public Color color;
+    public string content;
+    public Transform trans;
+
+    Text textBox;
+    private Vector2 pos;
+    void Start()
+    {
+        if (trans != null)
+        {
+            pos = Camera.main.WorldToScreenPoint(trans.position);
+        }
+        else
+        {
+            pos = textBox.rectTransform.anchoredPosition;
+        }
+        textBox = transform.GetComponent<Text>();
+        textBox.text = content;
+        textBox.color = color;
+        textBox.rectTransform.anchoredPosition = pos;
+        textBox.rectTransform.anchoredPosition = new Vector2(textBox.rectTransform.anchoredPosition.x, textBox.rectTransform.anchoredPosition.y + distance);
+    }
+
+    float fadeTimer = 1;
+    void Update()
+    {
+        if (fadeTimer >= 0)
+        {
+            fadeTimer -= Time.deltaTime;
+            textBox.color = new Color(color.r, color.g, color.b, fadeTimer);
+            textBox.rectTransform.anchoredPosition = new Vector2(textBox.rectTransform.anchoredPosition.x, textBox.rectTransform.anchoredPosition.y + (1 - fadeTimer));
+        }
+        if (fadeTimer < 0)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+}
