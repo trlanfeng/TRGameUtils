229fd663d829d18425450d3c308227e6dfde4cfc
孤月蓝风 <trlanfeng@foxmail.com>
2015-11-24 11:59:18 +0800

    添加新的颜色选择器

---
+++ Assets/ColorPicker/_Sample/ColorPickerSample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorPickerSample : MonoBehaviour {
+
+    ColorPick cp;
+	// Use this for initialization
+	void Start () {
+        cp = transform.GetComponent<ColorPick>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera.main.backgroundColor = cp.useColor;
+	}
+}
